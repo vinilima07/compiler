23c41926ece6043ab4ff82418260e2b1edac4b6d
Gustavo Zille <guzillaine@gmail.com>
2020-11-10 22:07:35 -0300

    Half of the syntactic functions.

--- src/syntactic/SyntacticParser.java
+++ src/syntactic/SyntacticParser.java
@@ -47,4 +47,211 @@ public class SyntacticParser {
         System.out.printf("deu erro kk %s", tokens.get(index));
     }
     
+    /**
+     * body ::= [declare decl-list] init stmt-list end
+     */
+    void body() {
+        if(tokens.get(index).type == TokenType.DECLARE) {
+            advance();
+            declList();
+        }
+        eat(TokenType.INIT);
+        stmtList();
+        eat(TokenType.END);
+    }
+
+    /**
+     * decl ::= ident-list : type
+     */
+    void decl() {
+        identList();
+        eat(TokenType.COLON);
+        type();
+    }
+
+    /**
+     * type ::= int | float | char
+     */
+    void type() {
+        switch(tokens.get(index).type) {
+            case INT:
+                eat(TokenType.INT); break;
+            case FLOAT:
+                eat(TokenType.FLOAT); break;
+            case CHAR:
+                eat(TokenType.CHAR); break;
+            default:
+                error(); break;
+        }
+    }
+
+    /**
+     * stmt ::= assign-stmt | if-stmt | while-stmt | repeat-stmt | read-stmt | write-stmt
+     */
+    void stmt() {
+        switch(tokens.get(index).type) {
+            case IDENTIFIER:
+                assignStmt(); break;
+            case IF:
+                ifStmt(); break;
+            case WHILE:
+                whileStmt(); break;
+            case REPEAT:
+                repeatStmt(); break;
+            case IN:
+                readStmt(); break;
+            case OUT:
+                writeStmt(); break;
+            default:
+                error(); break;
+        }
+    }
+
+    /**
+     * if-stmt ::= if condition then stmt-list if-stmt-pref
+     */
+    void ifStmt() {
+        eat(TokenType.IF);
+        condition();
+        eat(TokenType.THEN);
+        stmtList();
+        ifStmtPref();
+    }
+
+    /**
+     * if-stmt-pref ::= end | else stmt-list end
+     */
+    void ifStmtPref() {
+        switch(tokens.get(index).type) {
+            case END:
+                eat(TokenType.END); break;
+            case FLOAT:
+                eat(TokenType.ELSE);
+                stmtList();
+                eat(TokenType.END);
+                break;
+            default:
+                error(); break;
+        }
+    }
+
+    /**
+     * repeat-stmt ::= repeat stmt-list stmt-suffix
+     */
+    void repeatStmt() {
+        eat(TokenType.REPEAT);
+        stmtList();
+        stmtSuffix();
+    }
+
+    /**
+     * while-stmt ::= stmt-prefix stmt-list end
+     */
+    void whileStmt() {
+        stmtPrefix();
+        stmtList();
+        eat(TokenType.END);
+    }
+
+    /**
+     * readStmt ::= in "<<" identifier
+     */
+    void readStmt() {
+        eat(TokenType.IN);
+        eat(TokenType.LEFT_ARROW);
+        identifier();
+    }
+
+    /**
+     * writable ::= simple-expr | literal
+     */
+    void writable() {
+        switch(tokens.get(index).type) {
+            case NOT:
+            case SUB:
+            case IDENTIFIER:
+            case INTEGER_CONST:
+            case FLOAT_CONST:
+            case CHAR_CONST:
+            case OPEN_BRAC:
+                simpleExpr(); break;
+            case LITERAL:
+                eat(TokenType.LITERAL); break;
+            default:
+                error();
+        }
+    }
+
+    /**
+     * simple-expr ::= term simple-expr-esc
+     */
+    void simpleExpr() {
+        term();
+        simpleExprEsc();
+    }
+
+    /**
+     * simple-expr-esc ::= addop term simple-expr-esc | lambda
+     */
+    void simpleExprEsc() {
+        switch(tokens.get(index).type) {
+            case ADD:
+            case SUB:
+            case OR:
+                addop();
+                term();
+                simpleExprEsc();
+                break;
+        }
+    }
+
+    /**
+     * factor-a ::= factor | ! factor | "-" factor
+     */
+    void factorA() {
+        if(tokens.get(index).type == TokenType.NOT) {
+            eat(TokenType.NOT);
+        } else if(tokens.get(index).type == TokenType.SUB) {
+            eat(TokenType.SUB);
+        }
+        factor();
+    }
+
+    /**
+     * relop ::= "==" | ">" | ">=" | "<" | "<=" | "!="
+     */
+    void relop() {
+        switch(tokens.get(index).type) {
+            case EQUAL:
+                eat(TokenType.EQUAL); break;
+            case GREATER:
+                eat(TokenType.GREATER); break;
+            case GREATER_EQ:
+                eat(TokenType.GREATER_EQ); break;
+            case LESS:
+                eat(TokenType.LESS); break;
+            case LESS_EQ:
+                eat(TokenType.LESS_EQ); break;
+            case DIFF:
+                eat(TokenType.DIFF); break;
+            default:
+                error();
+        }
+    }
+
+    /**
+     * mulop ::= "*" | "/" | &&
+     */
+    void mulop() {
+        switch(tokens.get(index).type) {
+            case MUL:
+                eat(TokenType.MUL); break;
+            case DIV:
+                eat(TokenType.DIV); break;
+            case AND:
+                eat(TokenType.AND); break;
+            default:
+                error();
+        }
+    }
 }
